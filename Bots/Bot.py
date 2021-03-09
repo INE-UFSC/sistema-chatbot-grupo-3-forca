f1d1e4644c6c6246568d4169145ccf5c8b41ccbd
bnmfw <bernardoborgessandoval@gmail.com>
2021-03-09 10:53:05 -0300

    bot

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -5,7 +5,7 @@ import random as r
 
 class Bot(ABC):
 
-    def __init__(self. nome):
+    def __init__(self, nome, comandor, boas_vindas, despedida):
         self.nome = nome
         self.comandos = {"cmd":"valor"}
         self.boas_vindas = "string de boas vindas"
