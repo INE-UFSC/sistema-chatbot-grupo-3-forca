2486973c54597a79b525940c2137654754479fcc
Arthur Lourenço <arthurjolo@gmail.com>
2021-04-06 11:21:02 -0300

    grellert sendo lista

--- Bots/BotGrellet.py
+++ Bots/BotGrellet.py
@@ -1,29 +1,17 @@
 from Bots.Bot import Bot
+from Comando import Comando
 
 
 class BotGrellet(Bot):
     def __init__(self, nome):
-        comandos = {"": "",
-                    "Professor, posso fazer individual? :(": "Hahaha, jamais, o objetivo eh justamente aprender a trabalhar em grupo ;)"}
+        comandos = [
+            Comando(1, "Professor, posso fazer individual? :(", ["Hahaha, jamais, o objetivo eh justamente aprender a trabalhar em grupo ;)"])
+
+        ]
         super().__init__(nome, comandos)
 
     def apresentacao(self):
         print("Ola queridos, sou o prof. Mateus, leciono POO2 com meu caro, Jonata, e dou lab de SD\n")
-
-    def mostra_comandos(self):
-        i = 0
-        for comando in self.comandos:
-            i += 1
-            print(str(i)+". "+comando+"\n")
-
-
-    def executa_comando(self, cmd):
-        i = 1
-        for chave in self.comandos:
-            if cmd == i:
-                print(self.comandos[chave])
-            i += 1
-
 
     def boas_vindas(self):
         print("Ola alunes, so um segundo, deixa eu pegar meu cafezinho\nOpa, voltei, como estamos?")
