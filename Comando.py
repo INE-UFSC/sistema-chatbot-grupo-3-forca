aa7f676358e837e144be15fa2821dda430513aae
bnmfw <bernardoborgessandoval@gmail.com>
2021-04-06 10:20:52 -0300

    arquivo completo?

--- Comando.py
+++ Comando.py
@@ -1,1 +1,27 @@
-#codigo codigo codigo
+from random import randrange
+class Comando:
+    # recebe o id (inteiro), a mensagem e as respostas (opcional)
+    def __init__(self, id, msg, respostas=[]):
+        self.__id = id
+        self.__msg = msg
+        self.__respostas = []
+
+    @id.setter
+    def id(self):
+        return self.__id
+
+    @msg.setter
+    def msg(self):
+        return self.__msg
+
+    # retorna uma resposta aleatória
+    def getRandomResposta(self):
+        return self.__respostas[randrange(len(self.__respostas))]
+
+    # adiciona resposta
+    def addResposta(self, resposta):
+        self.__respostas.append(resposta)
+
+    # remove resposta (opcional)
+    def delResposta(self, resposta):
+        self.__respostas.remove(resposta)
