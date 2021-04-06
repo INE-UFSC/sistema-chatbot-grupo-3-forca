5ac59046f53a2f84c3508c5494011873fe16c498
Arthur Lourenço <arthurjolo@gmail.com>
2021-04-06 11:22:41 -0300

    grellert atualizado

--- Bots/BotGrellet.py
+++ Bots/BotGrellet.py
@@ -12,25 +12,7 @@ class BotGrellet(Bot):
 
     def apresentacao(self):
         print("Ola queridos, sou o prof. Mateus, leciono POO2 com meu caro, Jonata, e dou lab de SD\n")
-
-<<<<<<< HEAD
-=======
-    def mostra_comandos(self):
-        i = 0
-        for comando in self.comandos:
-            i += 1
-            print(str(i)+". "+comando+"\n")
-
-
-    def executa_comando(self, cmd):
-        i = 1
-        for comandos in self.comandos:
-            if comando.id == i:
-                print(comando.getRandomResposta())
-            i += 1
-
-
->>>>>>> 22e84e817530ea52a85b851e4db242d23d2e77f8
+        
     def boas_vindas(self):
         print("Ola alunes, so um segundo, deixa eu pegar meu cafezinho\nOpa, voltei, como estamos?")
 
