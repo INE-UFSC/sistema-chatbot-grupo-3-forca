999a44f5943c58af78e45389589b9db07a85e1dc
VictorValle-Git <victor.valle@grad.ufsc.br>
2021-04-13 11:04:12 -0300

    s1

--- Bots/Botview.py
+++ Bots/Botview.py
@@ -2,14 +2,15 @@ import PySimpleGUI as sg
 
 class BotView:
     def __init__(self, ):
-        self.__container []
+        self.__container = []
         self.__window = sg.Window("SistemaChatBot", self.__container ,font=("Helvetica", 16))
 
 
     def tela_consulta(self):
         linha0 = [sg.Text('Escolha um Bot')]
-        linha1 = [sg.Button('Importar')]
-        self.__container = [linha0, linha1]
+        linha1 = [sg.Button('Bot Grellert'), sg.Button('Bot Tyska'),sg.Button('Bot aluno exausto')]
+        linha2 = [sg.Button('Importar'), sg.Button('Exportar')]
+        self.__container = [linha0, linha1, linha2]
     
     def mostra_resultado(self, resultado):
         self.__Element('NAO SEI O QUE É AQUI').Update(restultado)
